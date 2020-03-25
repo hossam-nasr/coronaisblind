aee348ca744567c4abb88bc00139270c43187392
Hossam <hossam.nasr99@gmail.com>
2020-03-25 07:02:36 +0100

    Updates

---
+++ precache-manifest.61830d2cb7adcde0ed239f8f23d89e7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8bac178251216b48b898ce2f851836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5119faea94bbbfe5f7ef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abf2450f3d136eda1552",
+    "url": "/static/js/2.1c7e7c45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c7e7c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5119faea94bbbfe5f7ef",
+    "url": "/static/js/main.924fcb7b.chunk.js"
+  },
+  {
+    "revision": "87482e9086878457b51e",
+    "url": "/static/js/runtime-main.eefbf19a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
