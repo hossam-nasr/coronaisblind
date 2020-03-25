826a843344a786015f8f21e1586b2f3888cd56ef
Hossam <hossam.nasr99@gmail.com>
2020-03-25 07:41:27 +0100

    add theme

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    background: "#120B3F",
+    primary: "#120B3F",
+    secondary: "#5C0E73",
+    tertiary: "#37096C",
+    gold: "#EDBF2B",
+    silver: "#E5DEFF",
+    beige: "#E8CF8C",
+    maroon: "#7E6E23",
+    lightGold: "#B2AB99"
+  }
+};
+
+export default theme;
