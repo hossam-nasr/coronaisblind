267664978c82bc19c1c568f22d6e1dfe06e96faa
Hossam <hossam.nasr99@gmail.com>
2020-04-03 00:57:54 +0200

    clean up file structure

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "../../App.css";
+import { Link } from "react-router-dom";
+
+const NavBar = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light nav">
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse container" id="navbarNav">
+      <Link to="/" className="navbar-brand">
+        Corona is Blind
+      </Link>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link active">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </li>
+      </ul>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="/login">
+            Login
+          </a>
+        </li>
+        <li className="nav-item">
+          <Link to="/signup" className="nav-link">
+            Sign Up
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default NavBar;
---
+++ src/screens/SignUpScreen/index.js
@@ -0,0 +1,49 @@
+import React, { useCallback } from "react";
+import { SignUp } from "./styles";
+import app from "../../firebase";
+
+const SignUpScreen = ({ history }) => {
+  const handleSignUp = useCallback(
+    async event => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await app
+          .auth()
+          .createUserWithEmailAndPassword(email.value, password.value);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
+
+  return (
+    <div className="container">
+      <SignUp>
+        <div className="jumbotron">
+          <h2>Sign Up Now!</h2>
+          <form onSubmit={handleSignUp}>
+            <div className="form-group">
+              <label>Email address</label>
+              <input name="email" type="email" className="form-control" />
+              <small id="emailHelp" className="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input name="password" type="password" className="form-control" />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </SignUp>
+    </div>
+  );
+};
+
+export default SignUpScreen;
