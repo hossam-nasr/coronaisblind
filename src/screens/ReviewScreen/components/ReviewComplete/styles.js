9816404151869e07f83cf649ac320e10b6632342
Hossam <hossam.nasr99@gmail.com>
2020-04-25 16:15:00 -0500

    add review complete screen

---
+++ src/screens/ReviewScreen/components/ReviewComplete/styles.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import { themeGet } from "@styled-system/theme-get";
+import { Link } from "react-router-dom";
+
+export const Container = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  flex: 1;
+`;
+
+export const Title = styled.div`
+  text-align: center;
+  font-size: ${themeGet("fonts.title.size")};
+  font-family: ${themeGet("fonts.title.family")};
+  font-weight: ${themeGet("fonts.title.weight")};
+  line-height: ${themeGet("fonts.title.lineHeight")};
+  letter-spacing: ${themeGet("fonts.title.letterSpacing")};
+  color: ${themeGet("colors.textTitle")};
+  padding: 5vh;
+`;
+
+export const Subtitle = styled.div`
+  text-align: center;
+  font-size: ${themeGet("fonts.subtitle.size")};
+  font-family: ${themeGet("fonts.subtitle.family")};
+  font-weight: ${themeGet("fonts.subtitle.weight")};
+  line-height: ${themeGet("fonts.subtitle.lineHeight")};
+  letter-spacing: ${themeGet("fonts.subtitle.letterSpacing")};
+  color: ${themeGet("colors.textSubtitle")};
+  font-style: italic;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 20vh;
+`;
+
+export const StyledRouterLink = styled(Link)`
+  text-decoration: none;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
+
+export const Button = styled.div`
+  text-align: center;
+  background-color: ${themeGet("colors.secondary")};
+  color: ${themeGet("colors.tertiary")};
+  border: none;
+  font-size: 2vh;
+  padding: 2vh;
+  border-radius: 2vh;
+  margin: 5vh 0 0 0;
+  cursor: pointer;
+`;
