2ed328dea9ce01c67a986b682d9ba9b2a9cdf77d
Hossam <hossam.nasr99@gmail.com>
2020-04-25 06:07:34 -0500

    add new theme fonts and colors from .psd files

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,7 @@ const theme = {
     primary: "#120B3F",
     secondary: "#9c88ff",
     tertiary: "#f5f6fa",
+    quaternary: "#f47fae",
     gold: "#EDBF2B",
     silver: "#E5DEFF",
     beige: "#E8CF8C",
@@ -11,8 +12,32 @@ const theme = {
     lightGold: "#B2AB99",
     error: "#c40300",
     border: "#FFC107",
-    transparentCard: "rgba(156, 136, 255, 0.2)"
+    transparentCard: "rgba(156, 136, 255, 0.2)",
+    textTitle: "#F0C620",
+    textSubtitle: "#FFFFFF"
   },
+  fonts: {
+    title: {
+      family: "Gravity, sans-serif",
+      size: "50px",
+      weight: 700,
+      lineHeight: "60px",
+      letterSpacing: "5px"
+    },
+    subtitle: {
+      family: `"Gravity - Ultra Light Italic", Gravity, sans-serif`,
+      size: "30px",
+      weight: 400,
+      lineHeight: "40px",
+      letterSpacing: "3px"
+    },
+    textNormal: {
+      family: `"Gravity - Light", Gravity, sans-serif`,
+      weight: 300,
+      lineHeight: "40px",
+      letterSpacing: "4px"
+    }
+  }
 };
 
 export default theme;
