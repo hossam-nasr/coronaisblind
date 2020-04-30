1a75462ffe22531d3258f8347650d8e608b115d5
Mostafa Elgayar <meeger99@gmail.com>
2020-04-30 20:53:46 +0200

    added bouncing arrow and about text

--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../Auth.js";
 import { getCallList } from "../../helpers";
 import CallsList from "./components/CallsList";
-import { Container, Title, Subtitle, CallListContainer, Image } from "./styles";
+import { Container, Title, Subtitle, CallListContainer, Image, About, Arrow } from "./styles";
 import logo from "../../assets/img/ciblogo.png"
 
 const MainScreen = () => {
@@ -30,6 +30,9 @@ const MainScreen = () => {
         </CallListContainer>
       }
       {!currentUser && <Image src={logo} />}
+      {!currentUser && <Subtitle>COLLEGE QUARANTINE EDITION</Subtitle>}
+      {!currentUser && <About>Corona is Blind: College Quarantine Edition is a1-week event designed to help college students meet one another while remaining at home during the spread of COVID-19. Based on the popular TV series “Love is Blind,” the experience consists of a series of phone and video calls between college students from all over the nation.</About>}
+      {!currentUser && <Arrow></Arrow>}
     </Container>
   );
 };
