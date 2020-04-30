354af977332f3de96debb404ae08e1fc887a7b12
Mostafa Elgayar <meeger99@gmail.com>
2020-04-30 03:05:38 +0200

    centred logo

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 
 export const Nav = styled.nav`
   display: flex;
-  flex-direction: row;
-  padding: 0;
   border-bottom: 2px solid ${themeGet("colors.border")};
   justify-content: space-between;
   flex-wrap: wrap;
@@ -13,11 +11,19 @@ export const Nav = styled.nav`
 `;
 
 export const LeftSec = styled.div`
-  margin: 5vh;
+  margin: 2xvh;
+  flex: 1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 `;
 
 export const RightSec = styled.div`
-  margin: 5vh;
+  margin: 2vh;
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 `;
 
 export const NavItem = styled.span`
@@ -32,11 +38,13 @@ export const NavItem = styled.span`
 
 export const Logo = styled.div`
   list-style: none;
-  display: inline;
   font-size: 3vh;
   color: ${themeGet("colors.tertiary")};
-  margin: 5vh;
-  align-self: center;
+  margin: 2vh;
+  flex: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Rabet = styled(Link)`
