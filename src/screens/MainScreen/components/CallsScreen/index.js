4acff299550c9845ffe5e54b4032ec062a4474cf
Hossam <hossam.nasr99@gmail.com>
2020-05-01 02:33:08 -0500

    added new fonts and styles to CallsScreen

--- src/screens/MainScreen/components/CallsScreen/index.js
+++ src/screens/MainScreen/components/CallsScreen/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import CallsList from "./components/CallsList";
-import { Title, Subtitle, CallListContainer } from "./styles";
+import { Title, Subtitle, CallListContainer, SubSubTitle } from "./styles";
 
 const CallsScreen = ({ callList, name }) => (
   <>
     <Title>{`Hey, ${name}!`}</Title>
     <Subtitle>Welcome to Corona is Blind Season 3 Episode 2</Subtitle>
-    <Subtitle>This week on Corona is Blind...</Subtitle>
+    <SubSubTitle>This week on Corona is Blind...</SubSubTitle>
     <CallListContainer>
       <CallsList callList={callList} />
     </CallListContainer>{" "}
