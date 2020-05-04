aea10b417176a168942de2e041f8c1ce23ba305a
Mostafa Elgayar <meeger99@gmail.com>
2020-05-04 07:12:16 +0200

    added google analytics

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,17 @@ import "firebase/firestore";
 
 // TODO: WARNING: CONFIGURATION HAS TO BE SET TO ENVIRONMENT BEFORE PRODUCTION YA2EMA HANROO7 FE DAHYA
 
-const app = firebase.initializeApp({
+let app = firebase.initializeApp({
   apiKey: "AIzaSyAkgDEsFAcdLF0k-_SlEUIAEuvhDlRfPEc",
   authDomain: "coronaisblind-1e81a.firebaseapp.com",
   databaseURL: "https://coronaisblind-1e81a.firebaseio.com",
   projectId: "coronaisblind-1e81a",
   storageBucket: "coronaisblind-1e81a.appspot.com",
   messagingSenderId: "267268697534",
-  appId: "1:267268697534:web:160b264b03f91e72909def"
+  appId: "1:267268697534:web:160b264b03f91e72909def",
+  measurementId: "G-JKSBG9JTVN"
 });
+
+firebase.analytics();
 
 export default app;
